4bf335b99e3176e197c4b77a9e02d9da1a5a64b9
datianshi <dsz0111@gmail.com>
2015-03-08 19:48:17 -0400

    Add a main class as driver

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
-// create user in uaa
-func CreateUser() string {
-	return "abc"
-}
+import (
+	"fmt"
+	"os"
+
+	uaa "github.com/pivotalservices/uaaldapimport/adduser"
+	cc "github.com/pivotalservices/uaaldapimport/cloudcontroller"
+	config "github.com/pivotalservices/uaaldapimport/config"
+	. "github.com/pivotalservices/uaaldapimport/functions"
+	token "github.com/pivotalservices/uaaldapimport/token"
+)
 
 func main() {
+	err := run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
+func run() (err error) {
+	file, err := os.Open("config/fixtures/users.yml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	cfg, err := config.Parse(file)
+	if err != nil {
+		return
+	}
+	info := &Info{
+		Uaaurl:   "https://uaa.sys.homelab.io",
+		Ccurl:    "https://api.sys.homelab.io",
+		Clientid: "bulkimport",
+		Secret:   "test",
+	}
+	fun := token.GetToken.MapUsers(cfg.Users).AddUaaUser(uaa.Adduser).AddCCUser(cc.Adduser).MapOrgs(cc.AssociateOrg).MapSpaces(cc.AssociateSpace)
+	//fun := token.GetToken.MapUsers(cfg.Users)
+	return fun(info)
 }
