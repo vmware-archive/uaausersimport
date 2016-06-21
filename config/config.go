14c163ec2f1e401fd3f7fdbeb4febd706fbde765
Mark Alston <malston@pivotal.io>
2016-06-20 19:06:59 -0600

    Refactor config package

--- config/config.go
+++ config/config.go
@@ -3,7 +3,10 @@ package config
 import (
 	"io"
 	"io/ioutil"
+	"net/http"
 
+	uaa "github.com/cloudfoundry-incubator/uaa-token-fetcher"
+	"github.com/pivotal-golang/lager"
 	"gopkg.in/yaml.v2"
 )
 
@@ -11,6 +14,7 @@ type Space struct {
 	Name  string
 	Roles []string
 }
+
 type Org struct {
 	Name   string
 	Roles  []string
@@ -23,9 +27,23 @@ type User struct {
 	Emails     []string
 	Orgs       []Org
 }
+
 type Config struct {
 	Origin string
 	Users  []User
+}
+
+// Context DOCUMENT ME!
+type Context struct {
+	Ccurl        string
+	UAAURL       string
+	Clientid     string
+	Secret       string
+	Origin       string
+	RequestFn    func(string, string, string, string, io.Reader) (*http.Response, error)
+	TokenFetcher uaa.TokenFetcher
+	Users        []User
+	Logger       lager.Logger
 }
 
 func Parse(reader io.Reader) (*Config, error) {
--- config/config_test.go
+++ config/config_test.go
@@ -1,7 +1,6 @@
 package config_test
 
 import (
-	"fmt"
 	"os"
 
 	. "github.com/onsi/ginkgo"
@@ -15,7 +14,6 @@ var _ = Describe("Config", func() {
 		Context("with correct fixture", func() {
 			It("should be return correct config", func() {
 				cfg, err := config.Parse(file)
-				fmt.Print(err)
 				Ω(err).Should(BeNil())
 				Ω(len(cfg.Users)).Should(Equal(2))
 				Ω(cfg.Users[0].Externalid).Should(Equal("uid=jcalabrese,ou=People,dc=homelab,dc=io"))
@@ -23,11 +21,11 @@ var _ = Describe("Config", func() {
 				Ω(cfg.Users[0].Emails[0]).Should(Equal("jcalabrese@pivotal.io"))
 				Ω(cfg.Users[1].Emails[0]).Should(Equal("rparrish@pivotal.io"))
 				Ω(len(cfg.Users[0].Orgs)).Should(Equal(2))
-				Ω(cfg.Users[0].Orgs[0].Roles[0]).Should(Equal("managers"))
-				Ω(cfg.Users[0].Orgs[0].Roles[1]).Should(Equal("auditors"))
+				Ω(cfg.Users[0].Orgs[0].Roles[0]).Should(Equal("OrgManager"))
+				Ω(cfg.Users[0].Orgs[0].Roles[1]).Should(Equal("OrgAuditor"))
 				Ω(len(cfg.Users[0].Orgs[0].Spaces)).Should(Equal(2))
-				Ω(cfg.Users[0].Orgs[0].Spaces[0].Roles[0]).Should(Equal("managers"))
-				Ω(cfg.Users[0].Orgs[0].Spaces[1].Roles[1]).Should(Equal("auditors"))
+				Ω(cfg.Users[0].Orgs[0].Spaces[0].Roles[0]).Should(Equal("SpaceManager"))
+				Ω(cfg.Users[0].Orgs[0].Spaces[1].Roles[1]).Should(Equal("SpaceAuditor"))
 				Ω(cfg.Origin).Should(Equal("ldap"))
 				Ω(len(cfg.Users[0].Orgs[1].Spaces)).Should(Equal(2))
 			})
